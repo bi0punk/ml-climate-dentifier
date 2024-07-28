3224b644396ab008b75c45d067784164c458d10c
sysbot <sysbot@kali>
2024-07-28 18:45:18 -0400

    tunning

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 import os
+import time
 
 # Ruta al modelo entrenado
-model_path = 'day_evening_night_classifier_20240727.h5'
+model_path = 'day_evening_night_classifier_20240728.h5'
 
 # Cargar el modelo
 model = load_model(model_path)
@@ -16,18 +17,27 @@ labels = ['Day', 'Evening', 'Night']
 # URL de la cámara IP
 ip_camera_url = 'rtsp://admin:191448057devops@192.168.1.92:554/onvif1'
 
-# Probar con 'udp'
+# Configurar rtsp_transport a 'tcp'
 os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
 
-# Inicializar la captura de video
-cap = cv2.VideoCapture(ip_camera_url, cv2.CAP_FFMPEG)
+# Intentar abrir la cámara IP
+def open_video_capture(url):
+    cap = cv2.VideoCapture(url, cv2.CAP_FFMPEG)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 2)  # Ajustar el tamaño del buffer
+    if not cap.isOpened():
+        print("Error al abrir la cámara IP.")
+        return None
+    return cap
 
-if not cap.isOpened():
-    print("Error al abrir la cámara IP.")
-    exit()
+# Inicializar la captura de video
+cap = open_video_capture(ip_camera_url)
 
 # Dimensiones de la imagen
 img_height, img_width = 150, 150
+
+# Configuración de VideoWriter para guardar en H.265
+fourcc = cv2.VideoWriter_fourcc(*'HEVC')
+out = cv2.VideoWriter('output_h265.mp4', fourcc, 20.0, (640, 480))
 
 def predict_frame(frame):
     # Preprocesar la imagen
@@ -43,10 +53,17 @@ def predict_frame(frame):
     return predicted_label, confidence
 
 while True:
+    if cap is None:
+        time.sleep(1)
+        cap = open_video_capture(ip_camera_url)
+        continue
+
     ret, frame = cap.read()
     if not ret:
         print("Error al capturar el frame.")
-        break
+        cap.release()
+        cap = open_video_capture(ip_camera_url)
+        continue
     
     predicted_label, confidence = predict_frame(frame)
     
@@ -54,9 +71,9 @@ while True:
     text = f"{predicted_label}: {confidence:.2f}%"
     
     # Calcular la posición del texto en el lado derecho
-    text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 2, 3)[0]
-    text_x = frame.shape[1] - text_size[0] - 20
-    text_y = 60
+    text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 3, 4)[0]
+    text_x = frame.shape[1] - text_size[0] - 30
+    text_y = 100
     
     cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3, cv2.LINE_AA)
     
@@ -66,10 +83,14 @@ while True:
     # Mostrar la imagen
     cv2.imshow('IP Camera', frame_resized)
     
+    # Guardar el frame en el archivo de video
+    out.write(frame_resized)
+    
     # Salir con la tecla 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Liberar recursos
 cap.release()
+out.release()
 cv2.destroyAllWindows()
