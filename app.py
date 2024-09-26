a0e4cd0fb2509b815960ad2ee5d19b74b84013dc
bi0punk <systecinfo_cl@criptext.com>
2024-09-26 00:10:01 -0300

    fix some ..

--- app.py
+++ app.py
@@ -6,19 +6,17 @@ import os
 import time
 
 # Ruta al modelo entrenado
-model_path = 'day_evening_night_clear_cloudy_partly_cloudy_classifier_20240923.h5'
+MODEL_PATH = 'day_evening_night_clear_cloudy_partly_cloudy_classifier_20240923.h5'
 
 # Cargar el modelo
-model = load_model(model_path)
+model = load_model(MODEL_PATH)
 
 # Definir las etiquetas
-time_of_day_labels = ['Day', 'Evening', 'Night']
-weather_labels = ['Clear', 'Cloudy', 'Partly Cloudy']
+TIME_OF_DAY_LABELS = ['Day', 'Evening', 'Night']
+WEATHER_LABELS = ['Clear', 'Cloudy', 'Partly Cloudy']
 
 # URL de la cámara IP
-ip_camera_url = 'rtsp://admin:191448057devops@192.168.1.92:554/onvif1'
-
-
+IP_CAMERA_URL = 'rtsp://admin:191448057devops@192.168.1.92:554/onvif1'
 
 # Configurar rtsp_transport a 'tcp'
 os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
@@ -29,83 +27,73 @@ def open_video_capture(url):
     if not cap.isOpened():
         print("Error al abrir la cámara IP.")
         return None
-    cap.set(cv2.CAP_PROP_BUFFERSIZE, 10)  # Ajustar el tamaño del buffer
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 10)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
     cap.set(cv2.CAP_PROP_FPS, 30)
     return cap
 
 # Inicializar la captura de video
-cap = open_video_capture(ip_camera_url)
+cap = open_video_capture(IP_CAMERA_URL)
 
 # Dimensiones de la imagen
-img_height, img_width = 150, 150
+IMG_HEIGHT, IMG_WIDTH = 150, 150
 
-# Configuración de VideoWriter para guardar en un formato compatible
-fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Puedes probar con 'MJPG' si 'XVID' no funciona
+# Configuración de VideoWriter
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 def predict_frame(frame):
-    """ Realiza predicciones en el frame proporcionado. """
-    img = cv2.resize(frame, (img_height, img_width))
+    """Realiza predicciones en el frame proporcionado."""
+    img = cv2.resize(frame, (IMG_HEIGHT, IMG_WIDTH))
     img_array = img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     
-    # Realizar la predicción
     predictions = model.predict(img_array)[0]
     
-    # Asumimos que las primeras 3 salidas son para tiempo del día y las últimas 3 para el clima
-    time_of_day_pred = predictions[:3]   # Primeras 3 predicciones
-    weather_pred = predictions[3:]       # Últimas 3 predicciones
+    # Obtener predicciones
+    time_of_day_pred = predictions[:3]
+    weather_pred = predictions[3:]
     
-    # Obtener las etiquetas y sus probabilidades
-    time_of_day_label = time_of_day_labels[np.argmax(time_of_day_pred)]
-    weather_label = weather_labels[np.argmax(weather_pred)]
+    time_of_day_label = TIME_OF_DAY_LABELS[np.argmax(time_of_day_pred)]
+    weather_label = WEATHER_LABELS[np.argmax(weather_pred)]
     
-    # Calcular la confianza
     time_of_day_confidence = np.max(time_of_day_pred) * 100
     weather_confidence = np.max(weather_pred) * 100
     
     return time_of_day_label, time_of_day_confidence, weather_label, weather_confidence
 
 def draw_predictions(frame, time_of_day_label, time_of_day_confidence, weather_label, weather_confidence):
-    """ Dibuja las predicciones en el frame. """
-    # Configuración de fuentes y colores
+    """Dibuja las predicciones en el frame."""
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = 1.5
     font_color = (0, 255, 0)
     font_thickness = 3
     line_type = cv2.LINE_AA
     
-    # Preparar el texto
     text_time_of_day = f"Time of Day: {time_of_day_label}: {time_of_day_confidence:.2f}%"
     text_weather = f"Weather: {weather_label}: {weather_confidence:.2f}%"
     
     # Calcular la posición del texto
-    text_size_time_of_day = cv2.getTextSize(text_time_of_day, font, font_scale, font_thickness)[0]
-    text_size_weather = cv2.getTextSize(text_weather, font, font_scale, font_thickness)[0]
-    
-    text_x_time_of_day = frame.shape[1] - text_size_time_of_day[0] - 20
-    text_y_time_of_day = text_size_time_of_day[1] + 20
-    
-    text_x_weather = frame.shape[1] - text_size_weather[0] - 20
-    text_y_weather = text_y_time_of_day + text_size_weather[1] + 20
-    
-    # Dibujar el texto en el frame
-    cv2.putText(frame, text_time_of_day, (text_x_time_of_day, text_y_time_of_day), font, font_scale, font_color, font_thickness, line_type)
-    cv2.putText(frame, text_weather, (text_x_weather, text_y_weather), font, font_scale, font_color, font_thickness, line_type)
+    y_offset = 20
+    for text in [text_time_of_day, text_weather]:
+        text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
+        text_x = frame.shape[1] - text_size[0] - 20
+        text_y = y_offset + text_size[1]
+        cv2.putText(frame, text, (text_x, text_y), font, font_scale, font_color, font_thickness, line_type)
+        y_offset += text_size[1] + 20  # Espaciado entre líneas
 
 while True:
     if cap is None:
         time.sleep(1)
-        cap = open_video_capture(ip_camera_url)
+        cap = open_video_capture(IP_CAMERA_URL)
         continue
 
     ret, frame = cap.read()
     if not ret:
         print("Error al capturar el frame.")
         cap.release()
-        cap = open_video_capture(ip_camera_url)
+        cap = open_video_capture(IP_CAMERA_URL)
         continue
     
     # Realizar la predicción en el frame
@@ -114,7 +102,7 @@ while True:
     # Dibujar las predicciones en el frame
     draw_predictions(frame, time_of_day_label, time_of_day_confidence, weather_label, weather_confidence)
     
-    # Redimensionar la ventana
+    # Redimensionar la ventana para mostrar
     frame_resized = cv2.resize(frame, (640, 480))
     
     # Mostrar la imagen
